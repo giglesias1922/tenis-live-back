f98a725140f834eeb6c02cdf5bd88382a84caa72
Gustavo <giglesias@oriontech.com.ar>
2025-09-12 11:36:57 -0300

    Console log vribales mongo

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ const app = express();
 
 const port =  process.env.PORT || 3000;
 
-console.log("MONGO_URL",process.env.MONGO_URL);
+const mongo_url = process.env.MONGO_PUBLIC_URL || process.env.MONGO_URL;
+
+console.log("mongo_url",mongo_url);
 console.log("MONGO_DB_NAME",process.env.MONGO_DB_NAME);
 
 // Middleware para parsear JSON
 app.use(express.json());
 
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(mongo_url, {
   dbName: process.env.MONGO_DB_NAME
 })
   .then(() => console.log("✅ Conectado a MongoDB"))
